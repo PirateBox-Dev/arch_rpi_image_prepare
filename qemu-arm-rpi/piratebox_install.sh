1b755ff686a39ebf632392e7a84e23d7faccd142
Matthias Strubel <matthias.strubel@aod-rpg.de>
2015-12-03 20:33:50 +0100

    Automatize the customization of the final image   - Use arch-flag   - Dynamicall built missing packages   - Do final customization in an automatic manner

---
+++ qemu-arm-rpi/piratebox_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+  systemctl enable sshd
+  ln /usr/bin/python2 /usr/bin/python
+  cd root
+  wget http://downloads.piratebox.de/piratebox-ws_current.tar.gz
+  tar xzf piratebox-ws_current.tar.gz
+  cp -rv piratebox/piratebox /opt
+  groupadd nogroup && usermod -a -G nogroup nobody
+  /opt/piratebox/bin/install_piratebox.sh  /opt/piratebox/conf/piratebox.conf  part2
+  /opt/piratebox/bin/install_piratebox.sh  /opt/piratebox/conf/piratebox.conf imageboard
+  cp /prebuild/staging/piratebox.service  /etc/systemd/system 
+  cp /prebuild/staging/RPi_motd.txt /etc/motd
+
+
