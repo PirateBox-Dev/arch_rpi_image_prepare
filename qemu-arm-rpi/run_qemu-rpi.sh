3ae840fd84404fe15911fecdf4a3cd3ad0bdb871
Chris Landa <stylesuxx@gmail.com>
2016-01-15 16:52:45 +0100

    Updated image path to the new image name

--- qemu-arm-rpi/run_qemu-rpi.sh
+++ qemu-arm-rpi/run_qemu-rpi.sh
@@ -6,4 +6,4 @@ qemu-system-arm -kernel kernel-qemu-rpi -cpu arm1176 -m 256 \
   -M versatilepb -net nic -net user -redir tcp:2222::22 \
   -no-reboot -serial stdio \
   -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-  -hda ../raw_arch_image_file
+  -hda ../piratebox-rpi.img
