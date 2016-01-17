6b72a8dd1c431270575afa0e6873ea3009786cd8
Chris Landa <stylesuxx@gmail.com>
2016-01-17 21:00:15 +0100

    Improved starting of qemu for rpi

--- qemu-arm-rpi/rpi.sh
+++ qemu-arm-rpi/rpi.sh
@@ -2,8 +2,7 @@
 # Run the raw arch image in QEMU
 # SSH port of image is redirected to port 2222 of host machine
 
-qemu-system-arm -kernel kernel-qemu-rpi -cpu arm1176 -m 256 \
-  -M versatilepb -net nic -net user -redir tcp:2222::22 \
-  -no-reboot -serial stdio \
-  -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-  -hda ../piratebox-rpi.img
+qemu-system-arm -kernel kernel-qemu -M versatilepb -cpu arm1176 -m 256 \
+  -no-reboot -net nic -net user -redir tcp:2222::22 \
+  -drive file=../piratebox-rpi.img,index=0,media=disk,format=raw \
+  -append "root=/dev/sda2 rootfstype=ext4 rw panic=1"
