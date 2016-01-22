c899ece9ea317894a633589e74601e0dd2589fbc
Chris Landa <stylesuxx@gmail.com>
2016-01-22 19:38:44 +0100

    Fixed path to qemu kernel

--- qemu/rpi.sh
+++ qemu/rpi.sh
@@ -2,7 +2,7 @@
 # Run the raw arch image in QEMU
 # SSH port of image is redirected to port 2222 of host machine
 
-qemu-system-arm -kernel kernel-qemu -M versatilepb -cpu arm1176 -m 256 \
+qemu-system-arm -kernel kernel-qemu-rpi -M versatilepb -cpu arm1176 -m 256 \
   -no-reboot -net nic -net user -redir tcp:2222::22 \
   -drive file=../piratebox-rpi.img,index=0,media=disk,format=raw \
   -append "root=/dev/sda2 rootfstype=ext4 rw panic=1"
