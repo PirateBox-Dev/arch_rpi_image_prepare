a073c6bde029394ec2ade0b73118007cbd6e27ba
Chris Landa <stylesuxx@gmail.com>
2016-01-25 18:14:28 +0100

    Pass path to image

--- qemu/rpi.sh
+++ qemu/rpi.sh
@@ -4,5 +4,5 @@
 
 qemu-system-arm -kernel kernel-qemu-rpi -M versatilepb -cpu arm1176 -m 256 \
   -no-reboot -net nic -net user -redir tcp:2222::22 \
-  -drive file=../piratebox-rpi.img,index=0,media=disk,format=raw \
+  -drive file=$1,index=0,media=disk,format=raw \
   -append "root=/dev/sda2 rootfstype=ext4 rw panic=1"
