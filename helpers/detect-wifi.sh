b6c9ea4acf118d65834265c549cb7af538874623
Chris Landa <stylesuxx@gmail.com>
2016-02-01 19:40:20 +0100

    Added ARCH pkgbuild script to build newest hostapd version for 8188 and 8192

--- helpers/detect-wifi.sh
+++ helpers/detect-wifi.sh
@@ -6,7 +6,7 @@ CONFIG_TARGET="/opt/piratebox/conf/hostapd.conf"
 # Check if we have an nl80211 enabled device with AP mode, then we are done
 if iw list | grep > /dev/null "* AP$"; then
   echo "Found nl80211 device capable of AP mode..."
-  yes | pacman -U --needed "${PACKAGE_PATH}/hostapd-2.2-2-armv6h.pkg.tar.xz" > /dev/null
+  yes | pacman -U --needed "${PACKAGE_PATH}/hostapd-2*" > /dev/null
   cp "${CONFIG_PATH}/nl80211.conf" "${CONFIG_TARGET}"
   exit 0
 fi
@@ -14,7 +14,7 @@ fi
 # Check for r8188eu enabled device
 if dmesg | grep > /dev/null "r8188eu:"; then
   echo "Found r8188eu enabled device..."
-  yes | pacman -U --needed "${PACKAGE_PATH}/hostapd-8188eu-0.8-1-armv6h.pkg.tar.xz" > /dev/null
+  yes | pacman -U --needed "${PACKAGE_PATH}/hostapd-8188eu-*" > /dev/null
   cp "${CONFIG_PATH}/r8188eu.conf" "${CONFIG_TARGET}"
   exit 0
 fi
@@ -22,7 +22,7 @@ fi
 # Check for rtl8192cu enabled device
 if dmesg | grep > /dev/null "rtl8192cu"; then
   echo "Found rtl8192cu enabled device..."
-  yes | pacman -U --needed "${PACKAGE_PATH}/hostapd-8192cu-0.8_rtw_r7475.20130812_beta-3-armv6h.pkg.tar.xz" > /dev/null
+  yes | pacman -U --needed "${PACKAGE_PATH}/hostapd-8192cu-*" > /dev/null
   cp "${CONFIG_PATH}/rtl8192cu.conf" "${CONFIG_TARGET}"
   exit 0
 fi
