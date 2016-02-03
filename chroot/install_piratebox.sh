061cc213313e62128bcf22e14ad8b4faad07638e
Chris Landa <stylesuxx@gmail.com>
2016-02-03 23:09:14 +0100

    Touch the file that is responsible for setting the time

--- chroot/install_piratebox.sh
+++ chroot/install_piratebox.sh
@@ -21,3 +21,6 @@ sed -i 's:TIMESAVE_FORMAT="":TIMESAVE_FORMAT="+%C%g%m%d %H%M":' $CONF_PATH
 usermod -a -G nogroup minidlna
 chmod -R g+rw $PIRATEBOX_PATH/tmp
 chmod -R g+rw $PIRATEBOX_PATH/share
+
+# Touch the file where the time is saved to update it to the current time
+touch /var/lib/systemd/clock
