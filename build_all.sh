ee2824c12525cc8da439e6b90c1d86853676dcac
Matthias Strubel <matthias.strubel@aod-rpg.de>
2017-07-28 21:49:15 +0200

    Erease whitespace

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-set -e 
+set -e
 # Build images for:
 # * RPi 1
 # * RPi 2
 
 mkdir -p images
-for arch in "rpi" "rpi2" ; do 
+for arch in "rpi" "rpi2" ; do
   make dist ARCH=$arch
   sync
-  mv *.img.zip images 
+  mv *.img.zip images
   make clean ARCH=$arch
 done
