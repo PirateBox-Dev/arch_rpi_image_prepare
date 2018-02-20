64a381c8b220434ba9e08ca2c2d788f6c60f4715
Matthias Strubel <matthias.strubel@aod-rpg.de>
2018-02-20 14:14:17 +0100

    First set of changes to make pbxuser as a default user

--- chroot/configure_sudo.sh
+++ chroot/configure_sudo.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Configure sudo and add the alarm user
-groupadd sudo
+# Configure sudo and add the alarm user (just in case)
 echo '%sudo ALL=(ALL) ALL' >> /etc/sudoers
 usermod -a -G sudo alarm
--- chroot/install_piratebox.sh
+++ chroot/install_piratebox.sh
@@ -5,19 +5,30 @@ BIN_PATH=$PIRATEBOX_PATH/bin
 BUILD="$1"
 NEW_HOSTNAME="piratebox"
 
+PBX_USER="pbxuser"
+PBX_GRP="pbxuser"
+
 systemctl enable sshd
-groupadd nogroup && usermod -a -G nogroup nobody
 
 cd /root
 tar xzf *.tar.gz
 mv piratebox/piratebox /opt
 rm -r ./*.tar.gz
 
+# Adjust USER configuration in piratebox.conf
+sed -e "s|LIGHTTPD_USER=nobody|LIGHTTPD_USER=${PBX_USER}|g" \
+    -e "s|LIGHTTPD_GROUP=nogroup|LIGHTTPD_GROUP=${PBX_GRP}|g" \
+    -i "${CONF_PATH}"
+# Adjust user & group in lighttpd.conf
+sed -e "s|nobody|${PBX_USER}|g" \
+    -e "s|nogroup|${PBX_GRP}|g" \
+    -i "${PIRATEBOX_PATH}/conf/lighttpd/lighttpd.conf"
+
 $($BIN_PATH/install_piratebox.sh part2 > /dev/null)
 $($BIN_PATH/install_piratebox.sh imageboard > /dev/null)
 
 # Add minidlna user to nogroup and allow the group to read and write files
-usermod -a -G nogroup minidlna
+usermod -a -G ${PBX_GRP}  minidlna
 chmod -R g+rw $PIRATEBOX_PATH/tmp
 chmod -R g+rw $PIRATEBOX_PATH/share
 
@@ -29,6 +40,10 @@ touch /var/lib/systemd/clock
 # Place MOTD
 sed -i  -e "s|00-00-0000|${BUILD}|" "$PIRATEBOX_PATH/rpi/motd.txt"
 cp $PIRATEBOX_PATH/rpi/motd.txt /etc/motd
+
+# Create some default links to the PBXUSER home directory
+ln -s "${PIRATEBOX_PATH}/share/Shared"  "/home/${PBX_USER}/PirateBox_Shared"
+ln -s "${PIRATEBOX_PATH}/share/content" "/home/${PBX_USER}/PirateBox_www"
 
 # Set system's hostname to PirateBox
 echo "${NEW_HOSTNAME}" > /etc/hostname
